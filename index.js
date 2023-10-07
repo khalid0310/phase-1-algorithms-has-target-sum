ac593c0ff7aba2f287dcebb560ec9ceabe50531c
khalid <khalid.mohamed@moringaschool.com>
2023-10-07 18:16:18 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(nums, target) {
+  const numMap = {};
+
+  for (const num of nums) {
+      const diff = target - num;
+      if (numMap[diff]) {
+          return true;
+      }
+      numMap[num] = true;
+  }
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));   // true
+console.log(hasTargetSum([1, 2, 5], 4));            // false
